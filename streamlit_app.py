f2ca4b71239036a8801deda9dabe9ef230794b2f
PALG610 <Gurpreetpal788@gmail.com>
2025-06-26 14:16:00 +0200

    Update streamlit_app.py
    sf_df changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,6 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-
     st.write(my_insert_stmt)
     #st.stop()
     
@@ -44,5 +43,5 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
